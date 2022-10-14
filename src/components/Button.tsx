3798fd1ecb827a2ff028b08234ecf082693b3282
willianproni <wifepro@hotmail.com>
2022-10-13 22:42:22 -0300

    Criação da tela

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,19 +1,23 @@
-import { ReactNode } from 'react';
+import { ButtonHTMLAttributes, ReactNode } from 'react';
 import { Slot } from '@radix-ui/react-slot';
 import { clsx } from 'clsx'
 
-export interface ButtonProps {
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
     children?: ReactNode;
     asChild?: boolean;
+    className?: string;
 }
 
-export function Button({ children, asChild }: ButtonProps) {
+export function Button({ children, asChild, className, ...props }: ButtonProps) {
     const Comp = asChild ? Slot : 'button';
 
     return (
         <Comp className={clsx(
-            'rounded bg-cyan-500 text-back font-semibold py-4 px-3 text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white',
-        )}>
+            'rounded bg-cyan-500 text-back font-semibold py-3 px-4 text-sm w-full transition-colors hover:bg-cyan-300 focus:ring-2 ring-white',
+            className,
+        )}
+            {...props}
+        >
             {children}
         </Comp>
     )
